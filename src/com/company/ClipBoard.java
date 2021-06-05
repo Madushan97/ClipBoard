a51e6c220fde5672b630bb1bebed3847f119971f
Madushan Nipuna Shantharathna <madushannipuna1997@gmail.com>
2021-06-05 21:05:32 +0530

    1st commit

---
+++ src/com/company/ClipBoard.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class ClipBoard {
+
+    private String content;
+
+    private static ClipBoard clipBoard = new ClipBoard();
+
+    private ClipBoard(){
+        System.out.println("Clipboard object created");
+    }
+    public static ClipBoard getInstance(){
+
+        if(clipBoard == null){
+            clipBoard = new ClipBoard();
+        }
+        return clipBoard;
+    }
+
+    //copy
+    public  void  copy(String text){
+        this.content = text;
+    }
+    //paste
+    public void paste(){
+        System.out.println(this.content);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ClipBoard clipBoard1 =  ClipBoard.getInstance(); //objects
+        ClipBoard clipBoard2 =  ClipBoard.getInstance();
+
+        clipBoard1.copy("abc");
+        clipBoard1.paste();     //abc
+
+        clipBoard1.paste(); //abc
+
+        clipBoard2.copy("xyz");
+        clipBoard1.paste(); //xyz
+        clipBoard2.paste(); //xyz
+        clipBoard1.copy("pqr");
+        clipBoard2.paste(); //pqr
+
+
+    }
+}
